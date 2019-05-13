cbfa82fdbbe70f885a9667c51acce17436d72dbd
Elmira Majdi <bm.elmira@gmail.com>
2019-05-13 03:30:00 -0400

    comit build test

---
+++ build/precache-manifest.f33dc28debd7ac8062c605bbfbb2644d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "761bec11221cbd42ec478617e6fdf9f3",
+    "url": "/projectred/index.html"
+  },
+  {
+    "revision": "94919e4d8cdb0347fe48",
+    "url": "/projectred/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "793921127ceeb9802df1",
+    "url": "/projectred/static/css/main.2e92e69f.chunk.css"
+  },
+  {
+    "revision": "94919e4d8cdb0347fe48",
+    "url": "/projectred/static/js/2.628b4ab2.chunk.js"
+  },
+  {
+    "revision": "793921127ceeb9802df1",
+    "url": "/projectred/static/js/main.799dbbfc.chunk.js"
+  },
+  {
+    "revision": "1cb8642ce7283c1ef7df",
+    "url": "/projectred/static/js/runtime~main.c5655206.js"
+  }
+]);
